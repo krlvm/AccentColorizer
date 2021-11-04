d631a1e07b8544ac9c34aaea1c60068c78252033
krlvm <51774833+krlvm@users.noreply.github.com>
2021-11-04 18:57:59 +0300

    Initial commit (WIP)

---
+++ AccentColorizer/AccentColorHelper.cpp
@@ -0,0 +1,36 @@
+#include "AccentColorHelper.h"
+
+// https://social.msdn.microsoft.com/Forums/en-US/16b70775-d87e-42d3-aa8f-41d7d6888c66/how-to-get-colors-of-default-app-mode-quotdarkquot-in-my-win32-app?forum=windowsgeneraldevelopmentissues
+
+typedef DWORD(WINAPI* PGetImmersiveColorFromColorSetEx)(UINT dwImmersiveColorSet, UINT dwImmersiveColorType, bool bIgnoreHighContrast, UINT dwHighContrastCacheMode);
+typedef WCHAR** (WINAPI* PGetImmersiveColorNamedTypeByIndex)(UINT dwImmersiveColorType);
+typedef int (WINAPI* PGetImmersiveColorTypeFromName)(const WCHAR* name);
+typedef int (WINAPI* PGetImmersiveUserColorSetPreference)(bool bForceCheckRegistry, bool bSkipCheckOnFail);
+
+PGetImmersiveColorFromColorSetEx pGetImmersiveColorFromColorSetEx = NULL;
+PGetImmersiveColorTypeFromName pGetImmersiveColorTypeFromName = NULL;
+PGetImmersiveUserColorSetPreference pGetImmersiveUserColorSetPreference = NULL;
+
+
+COLORREF accent;
+COLORREF accentLight1;
+COLORREF accentLight2;
+COLORREF accentLight3;
+
+DWORD GetImmersiveColorRGB(const WCHAR* name)
+{
+	return pGetImmersiveColorFromColorSetEx(pGetImmersiveUserColorSetPreference(false, false), pGetImmersiveColorTypeFromName(name), false, 0);
+}
+
+void InitAccentColors()
+{
+	HMODULE hUxTheme = LoadLibrary(TEXT("uxtheme.dll"));
+	pGetImmersiveColorFromColorSetEx = (PGetImmersiveColorFromColorSetEx)GetProcAddress(hUxTheme, MAKEINTRESOURCEA(95));
+	pGetImmersiveColorTypeFromName = (PGetImmersiveColorTypeFromName)GetProcAddress(hUxTheme, MAKEINTRESOURCEA(96));
+	pGetImmersiveUserColorSetPreference = (PGetImmersiveUserColorSetPreference)GetProcAddress(hUxTheme, MAKEINTRESOURCEA(98));
+
+	accent = GetImmersiveColorRGB(L"ImmersiveSystemAccent");
+	accentLight1 = GetImmersiveColorRGB(L"ImmersiveSystemAccentLight1");
+	accentLight2 = GetImmersiveColorRGB(L"ImmersiveSystemAccentLight2");
+	accentLight3 = GetImmersiveColorRGB(L"ImmersiveSystemAccentLight3");
+}
---
+++ AccentColorizer/AccentColorHelper.h
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+extern COLORREF accent;
+extern COLORREF accentLight1;
+extern COLORREF accentLight2;
+extern COLORREF accentLight3;
+
+void InitAccentColors();
