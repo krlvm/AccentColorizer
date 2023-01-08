e175e0a2580025b54cbbd353f60b44e1a7083170
krlvm <51774833+krlvm@users.noreply.github.com>
2023-01-08 21:56:09 +0300

    Colorize scrollbars on Windows 7 (#22)

--- AccentColorizer/StyleModifier.cpp
+++ AccentColorizer/StyleModifier.cpp
@@ -291,6 +291,13 @@ void ModifyStyles() {
                 }
             }
         }
+        for (i = 1; i <= 10; i++)
+        {
+            for (k = 1; k <= 4; k++)
+            {
+                ModifyStyle(L"ScrollBar", i, 0, k);
+            }
+        }
     }
 }
 
